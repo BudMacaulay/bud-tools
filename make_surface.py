8ce8a38f41fbc54bc852655fd5a762ff041da5f0
BudMacaulay <55360869+BudMacaulay@users.noreply.github.com>
2022-06-06 12:42:07 +0100

    make_surface and stretch_cell
    surface creation and discriminate cell stretching.
    using the cli_run template and logging template.
    
    Should be cool

---
+++ make_surface.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+import sys, os, argparse, logging
+from pymatgen.core.surface import SlabGenerator
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen.core.surface import Structure, Lattice, get_symmetrically_distinct_miller_indices, \
+    get_symmetrically_equivalent_miller_indices
+import numpy as np
+
+c_log = logging.getLogger(__name__)
+# Adopted format: level - current function name - mess. Width is fixed as visual aid
+std_format = '[%(levelname)5s - %(funcName)10s] %(message)s'
+logging.basicConfig(format=std_format)
+c_log.setLevel(logging.WARNING)
+
+
+def make_surface(init_structure, miller_index=None, layer_size=7, vac_size=13, mode="move-sites"):
+    global c_log
+
+    # Guess the oxidation states to detect dipole
+    init_structure.add_oxidation_state_by_guess()
+
+    if miller_index is None:
+        c_log.warning("NO Miller index supplied defaulting to 0 0 1")
+        miller_index = [0, 0, 1]
+
+    if not all((type(x) == int for x in miller_index)):  # ERROR HANDLE
+        c_log.warning("Non-integer values in miller index - im not sure how pymatgen handles this so crashing")
+        return
+
+    sg = SpacegroupAnalyzer(init_structure)
+    c_log.debug("Initialising guess Oxidation States")
+    spec = [x for x in set(init_structure.species)]
+    c_log.info(f"Ox states are as: {' '.join(str(x) for x in spec)}")
+    c_log.info(f"Desired miller Index is: {miller_index}")
+    c_log.info(
+        f"Space Group is {sg.get_space_group_number()}, {sg.get_space_group_symbol()} {sg.get_crystal_system()}\n")
+
+    conv = sg.get_conventional_standard_structure()
+    if not conv == init_structure:
+        c_log.warning(f"Input structure is not in standard format for this space group")
+        c_log.warning(
+            f"proceeding to cut the slab using the miller plane from the current lattice - This may be undesired\n")
+
+    c_log.info(f"Within current structure: Symmetry equivalent indices to {miller_index}:\n"
+               f""
+               f"{get_symmetrically_equivalent_miller_indices(structure=init_structure,miller_index=miller_index)[1:]}"
+               f"\n")
+
+    t = SlabGenerator(initial_structure=init_structure, miller_index=miller_index, in_unit_planes=False,
+                      reorient_lattice=True, min_slab_size=layer_size, min_vacuum_size=vac_size,
+                      lll_reduce=True, center_slab=True, max_normal_search=max(miller_index))
+
+    if mode == "break-stio":
+        slabs = t.get_slabs(symmetrize=True, repair=True)
+    elif mode == "move-sites":
+        slabs = t.get_slabs(symmetrize=False, repair=True)
+    else:
+        c_log.warning(f"mode: {mode} not found, Exitting")
+        return
+
+    datafile = [["NAME", "SIZE", "Area", "Cnorm", "Symmetry", "Polarity", "Tasker Type"]]
+    work_on = []
+    c_log.info(f"Total number of initial slabs: {len(slabs)}")
+    for n, slab in enumerate(slabs):
+        slab.sort()
+        if not slab.is_polar() and slab.is_symmetric():
+            c_log.debug(f"Slab {n}: is Tasker Type 2")
+            fn = f"slabs/type2_{n}_{''.join(str(a) for a in slab.miller_index)}.vasp"
+            slab.to(fmt="poscar", filename=fn)
+        else:
+            c_log.debug(f"Slab {n}: is Tasker Type 3; saving and also adding to reconstruction list")
+            fn = f"slabs/type3_{n}_{''.join(str(a) for a in slab.miller_index)}.vasp"
+            slab.to(fmt="poscar", filename=fn)
+            sc = slab
+            sc.make_supercell(scaling_matrix=[2, 1, 1])
+            work_on.extend(sc.get_tasker2_slabs())
+        datafile.append(
+            [fn.split("/")[-1], len(slab), round(slab.surface_area, 3), round(slab.normal[2], 3), slab.is_symmetric(),
+             np.linalg.norm(slab.dipole / slab.surface_area)])
+
+    c_log.info(f"Total number of Tasker type 3 slabs: {len(work_on)}")
+    c_log.info(f"Proceeding to tasker 2 said slabs")
+    for n, slab in enumerate(work_on):
+        slab.sort()
+        if not slab.is_polar(tol_dipole_per_unit_area=1e-2) and slab.is_symmetric():
+            fn = f"slabs/tc_type2_{len(slabs) + n}_{''.join(str(a) for a in slab.miller_index)}.vasp"
+            slab.to(fmt="poscar", filename=fn)
+        else:
+            fn = f"slabs/failedtc_type3_{len(slabs) + n}_{''.join(str(a) for a in slab.miller_index)}.vasp"
+            slab.to(fmt="poscar", filename=fn)
+        datafile.append(
+            [fn.split("/")[-1], len(slab), round(slab.surface_area), round(slab.normal[2], 3), slab.is_symmetric(),
+             np.linalg.norm(slab.dipole / slab.surface_area)])
+
+    with open("slabs/make_surface.dat", "w+") as f_ile:
+        for line in datafile:
+            f_ile.writelines(" ".join(str(x) for x in line))
+
+
+def cli_run(argv):
+    """ Wrapper for the above command, this is basically a quitck and easy wrap for pymatgen stuff """
+    parser = argparse.ArgumentParser(description=make_surface.__doc__)
+
+    # Someone make argparse a little easier to read the docs before i scream #
+    parser.add_argument("bulk_poscar", type=str, help="BULK POSCAR YOU WISH TO SLICE")
+    parser.add_argument("-m", dest="millerplane", help="The miller plane you would like to slice across", nargs=3,
+                        type=int)
+
+    # Optional flags - these probably need some fiddling and are likely system dependant
+    parser.add_argument("-v", dest="vac", default=20, help="vaccuum size", type=float)
+    parser.add_argument("-l", dest="lay", default=11, type=float,
+                        help="layers to generate (in angstrom - Good values are 11+")
+    parser.add_argument("--mode", dest="recon_mode", default="move-sites", type=str,
+                        help="Mode to fix Tasker type 3 slabs, either move-sites (default) or break-stio.")
+
+    parser.add_argument("--verbose", dest="verbose", action="store_true")
+    parser.add_argument("--debug", dest="debug", action="store_true")
+    args = parser.parse_args(argv)
+
+    # Verbose setting
+    global c_log
+
+    if args.verbose:
+        c_log.setLevel(logging.INFO)
+    elif args.debug:
+        c_log.setLevel(logging.DEBUG)
+
+    c_log.debug(args)
+
+    os.makedirs("slabs/", exist_ok=True)
+    init_structure = Structure.from_file(args.bulk_poscar)
+    make_surface(init_structure, miller_index=args.millerplane, vac_size=args.vac, layer_size=args.lay,
+                 mode=args.recon_mode)
+
+    c_log.info("All done!")
+
+
+if __name__ == "__main__":
+    cli_run(sys.argv[1:])
---
+++ stretch_cell.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import sys
+import os, argparse, logging
+
+from pymatgen.core import Structure, Lattice
+from pymatgen.io.vasp import Poscar
+
+# Init global logger for this scope.
+c_log = logging.getLogger(__name__)
+logging.basicConfig(format='[%(levelname)5s - %(funcName)10s] %(message)s')
+c_log.setLevel(logging.WARNING) # Initialise logging level to warning, this will only print out on well, warnings
+
+def stretch_cell(structure, dimension, scale_amount, fix_bonds):
+    """ Method to stretch an input structure across a specified dimension with the optional choice to fix a particular bond length
+    Pymatgen does all the heavy lifting, although pymatgen is stupid so yeah
+    """
+    global c_log
+
+    c_log.info(f"Loaded Structure has: {len(structure)} sites")
+    c_log.info(f"Composition: {structure.composition.formula}")
+
+    l = list(structure.lattice.parameters)
+    l[dimension] = l[dimension] * (1.00 + scale_amount)
+    c_log.info(f"Initial Lattice dimensions: {l}")
+    c_log.info(f"Stretching dimension {dimension} by {1.00 + scale_amount} of original")
+    c_log.info(f"New Lattice dimensions: {l}")
+    ns = Structure(coords=structure.frac_coords, species=structure.species, lattice=Lattice.from_parameters(*l))
+
+    # Handle the edge case first
+    if fix_bonds is None:
+        return Poscar(ns)
+
+    # If bond lengths are to be fixed
+    c_log.info(f"Attempting to fix the bond lengths for {fix_bonds[0] and fix_bonds[1]}")
+    c_log.debug(f"Distance Matrix for initial structure:\n {structure.distance_matrix.round(2)}")
+    idx_fix2 = [(n,x) for n,x in enumerate(structure) if x.specie.value == fix_bonds[1]]
+
+    shift_list = []
+    for idx, site in idx_fix2: # Pair each speices in fix_bonds[1] with a species in fix_bonds[2]
+        neigh = structure.get_neighbors(site=site, r=3.5)
+        neigh = [x for x in neigh if x.specie.value == fix_bonds[0]]
+        x_idx = min(neigh, key=lambda x:x.nn_distance)
+        c_log.debug(f"Oxygen {site} nearest neighbour is {x_idx} with index {x_idx.index}")
+        c_shift = site.coords[dimension] - x_idx.coords[dimension] #Shift value
+        c_log.debug(f"With a shift value of: {c_shift}")
+        shift_list.append((idx, x_idx.index, c_shift))
+
+    for idx_1,idx_2, shift in shift_list:
+        c_log.debug(f"Original: {ns[idx_1].coords[dimension]}")
+        c_log.debug(f"Neighbour: {ns[idx_2].coords[dimension]}")
+        c_log.debug(f"Shift: {shift}")
+        # Stupidly Niave approach, cause why would changing the coords result in changing the coordss
+        # ns[idx_1].coords[dimension] = new_structure[idx_2].coords[dimension] + shift
+        new_xyz = [ns[idx_1].coords[0], ns[idx_1].coords[1], ns.coords[dimension] + shift]
+        c_log.debug(f"Final: {new_xyz[dimension]}")
+        ns.replace(i=idx_1, species=ns[idx_1].species, coords=new_xyz, coords_are_cartesian=True)
+
+
+    c_log.info("All finished up")
+
+    return Poscar(ns)
+
+
+def cli_run(argv):
+    """Wrapper and Handler of above calls"""
+
+    global c_log
+
+    parser = argparse.ArgumentParser(description=stretch_cell.__doc__, epilog="Buddy 22")
+    # Positional Args
+    parser.add_argument("filename", default="CONTCAR", type=str, nargs="?",
+                        help="Input file to load as a pymatgen structure")
+    parser.add_argument("dimension", default=2, type=int, nargs="1",
+                        help="Dimension in which to scale across")
+    parser.add_argument("scale_amount", default=0.05, type=float, nargs=1,
+                        help="Fractional Scale amount, defaults to 0.05 (5%)")
+
+    # Optional Args
+    parser.add_argument("--fix", dest="fix", default=None, type=list, nargs=2,
+                        help="Bonds in which to fix upon scaling (i.e stretch across the other bonds)")
+    # Loud Args
+    parser.add_argument("--verbose", action="store_true", dest="verbose",
+                        help="Loud printouts")
+    parser.add_argument("--debug", action="store_true", dest="debug",
+                        help="show debugging information, mostly if the code goes grossly wrong")
+    args = parser.parse_args(argv)
+
+    if args.verbose:
+        c_log.setLevel(level=logging.INFO)
+    elif args.debug:
+        c_log.setLevel(level=logging.DEBUG)
+
+    structure = Structure.from_file(filename=args.filename)
+    stretch_cell(structure, dimension=args.dimension, scale_amount=args.scale_amount, fix_bonds=args.fix)
+
+if __name__ == "__main__":
+    cli_run(sys.argv[1:])
